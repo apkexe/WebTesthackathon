92d014ee416802ad3723040dbfee2d031c7598ef
apkexe <strikos.kon@gmail.com>
2023-12-22 01:57:32 +0200

    added documentation files at /cypress/e2e

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  projectId: "oanozi",
   // ...rest of the Cypress project config
-}
+  projectId: "oanozi",
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};

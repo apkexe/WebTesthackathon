426ed29aa301e7409e680c40a325718d14057f07
WokeToThis <70851911+WokeToThis@users.noreply.github.com>
2023-12-22 15:09:26 +0200

    Update US1.cy.js

--- cypress/e2e/US1.cy.js
+++ cypress/e2e/US1.cy.js
@@ -16,6 +16,8 @@ describe('template spec', () => {
       cy.get(`.product:nth-child(${i}) .stepper-input input.quantity`).as(numInput); //every quantity
       cy.get(`.product:nth-child(${i}) .stepper-input a.decrement`).as(decrement);
       cy.get(`.product:nth-child(${i}) .stepper-input a.increment`).as(increment);
+
+      //this way we can easily have every element from products
     }
   })
 
@@ -24,13 +26,14 @@ describe('template spec', () => {
 
 
   it('US1_1', () => {
-    
+    //adding element to cart
 
     cy.get('@addToCartButton3').click();
 
   })
 
   it('US1_2', () => {
+    //adding multiple element to cart
     
     
       cy.get('@addToCartButton1').click();
@@ -45,6 +48,8 @@ describe('template spec', () => {
 
 
   it('US1_3', () => {
+    //adding multiple element to cart and checking if they exist in cart
+
 
     cy.get('@quantityInput20').clear().type('3');
     
@@ -68,6 +73,64 @@ describe('template spec', () => {
       // const text = $product.text();
       cy.get(`.cart-items .cart-item .amount`).should('contain', price1); //for 3 oranges
     });
+
+
+
+    
+  })
+
+
+  it('US1_4', () => {
+    //adding multiple element to cart and checking if they exist in cart
+    //and adding with sign minus 
+
+    cy.get('@decrementLink3').click();
+    cy.get('@decrementLink3').click();
+    cy.get('@decrementLink3').click();
+    cy.get('@incrementLink3').click();
+
+    cy.get('@addToCartButton3').click();
+
+
+    cy.get('@quantityInput20').clear().type('3');
+    cy.get('@addToCartButton20').click();
+    
+
+    // // to check cart
+    cy.get('.cart-icon').click();
+
+    //this is for product name 20 
+    cy.get(`.product:nth-child(20) .product-name`).then(($product) =>{
+      const text = $product.text();
+      cy.get(`.cart-items .cart-item .product-info .product-name`).should('contain', text);
+    });
+
+    //this is for product name 3 
+    cy.get(`.product:nth-child(3) .product-name`).then(($product) =>{
+      const text1 = $product.text();
+      cy.get(`.cart-items .cart-item .product-info .product-name`).should('contain', text1);
+    });
+    
+
+    //this is for product price20
+    cy.get(`.product:nth-child(20) .product-price`).then(($product) =>{
+
+      const pricee = parseInt($product.text());
+      const price11 = pricee*3;
+      // const text = $product.text();
+      cy.get(`.cart-items .cart-item .amount`).should('contain', price11); //for 3 oranges
+    });
+
+
+    //this is for product price3
+    cy.get(`.product:nth-child(3) .product-price`).then(($product) =>{
+
+      const price = parseInt($product.text());
+      const price1 = price*2;
+      // const text = $product.text();
+      cy.get(`.cart-items .cart-item .amount`).should('contain', price1); //for 3 cucumbers
+    });
+
 
     
   })

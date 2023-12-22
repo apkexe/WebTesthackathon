a66a12c1870794261ca7b6514f8b44a344fd17c8
apkexe <77940893+apkexe@users.noreply.github.com>
2023-12-22 15:36:32 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'oanozi',
-})
-
-
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
 

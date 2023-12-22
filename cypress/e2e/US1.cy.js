4152a63467b9cf6f480309a5012e091f0337c105
WokeToThis <70851911+WokeToThis@users.noreply.github.com>
2023-12-22 14:47:55 +0200

    Add files via upload

---
+++ cypress/e2e/US1.cy.js
@@ -0,0 +1,109 @@
+describe('template spec', () => {
+
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/cart')
+    for (let i = 1; i <= 29; i++) {
+      const aliasName = `addToCartButton${i}`; //this is for the add to cart button
+      const numInput = `quantityInput${i}`; //this is for quantity
+      const decrement = `decrementLink${i}`; //this is the minus sign
+      const increment = `incrementLink${i}`; //this is the minus sign
+      
+      cy.get(`.product:nth-child(${i}) .product-action button`).as(aliasName); //we get every element that has add to cart button
+      cy.get(`.product:nth-child(${i}) .stepper-input input.quantity`).as(numInput); //every quantity
+      cy.get(`.product:nth-child(${i}) .stepper-input a.decrement`).as(decrement);
+      cy.get(`.product:nth-child(${i}) .stepper-input a.increment`).as(increment);
+    }
+  })
+
+
+
+
+
+  it('US1_1', () => {
+    
+
+    cy.get('@addToCartButton3').click();
+
+  })
+
+  it('US1_2', () => {
+    
+    
+      cy.get('@addToCartButton1').click();
+      cy.get('@addToCartButton12').click();
+      cy.get('@addToCartButton13').click();
+      cy.get('@addToCartButton14').click();
+    
+    
+    
+    
+  })
+
+
+  it('US1_3', () => {
+
+    cy.get('@quantityInput20').clear().type('3');
+    
+    cy.get('@addToCartButton20').click();
+    
+
+    // // to check cart
+    cy.get('.cart-icon').click();
+
+    //this is for product name
+    cy.get(`.product:nth-child(20) .product-name`).then(($product) =>{
+      const text = $product.text();
+      cy.get(`.cart-items .cart-item .product-info .product-name`).should('contain', text);
+    });
+
+    //this is for product price
+    cy.get(`.product:nth-child(20) .product-price`).then(($product) =>{
+      const text = $product.text();
+      cy.get(`.cart-items .cart-item .product-info .product-price`).should('contain', text);
+    });
+
+    
+  })
+
+  
+
+
+    // cy.get('@quantityInput20').clear().type('3'); //testing to see if that's right
+    // cy.get('@decrementLink20').click();
+    // cy.get('@incrementLink21').click();
+
+   
+
+
+    // // US 1
+
+    // cy.get('@addToCartButton3').click();
+
+    // cy.get('@addToCartButton4').click();
+
+    // // // to check cart
+    // cy.get('.cart-icon').click();
+
+    // cy.get(`.product:nth-child(3) .product-name`).then(($product) =>{
+    //   const text = $product.text();
+    //   cy.get(`.cart-items .cart-item .product-info .product-name`).should('contain', text);
+    // });
+
+    
+    // const text =  cy.get('@productName3');
+    // cy.log(text);
+        // cy.get(`.cart-items .cart-item .product-info .product-name`).should('contain', '$productName3');
+
+   
+    // const text = cy.get('@productName3').text();
+
+
+
+  
+})
+
+
